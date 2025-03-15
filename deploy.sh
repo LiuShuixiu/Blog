b4e704a78b5a1d32e55e985fb8c3d5f47a59c08c
LiuShuixiu <1067487478@qq.com>
2025-03-15 19:53:56 +0800

    ''[D部署脚本[C['

--- deploy.sh
+++ deploy.sh
@@ -4,20 +4,21 @@
 set -e
 
 
-push_addr=`git remote get-url --push origin` # git提交地址，也可以手动设置，比如：push_addr=git@github.com:xugaoyi/vuepress-theme-vdoing.git
-commit_info=`git describe --all --always --long`
+push_addr=`git remote get-url --push io` # git提交地址，也可以手动设置，比如：push_addr=git@github.com:xugaoyi/vuepress-theme-vdoing.git
+# commit_info=`git describe --all --always --long`
 dist_path=docs/.vuepress/dist # 打包生成的文件夹路径
-push_branch=gh-pages # 推送的分支
+push_branch=main # 推送的分支
 
 # 生成静态文件
-npm run build
+npm run build:win
 
 # 进入生成的文件夹
 cd $dist_path
 
 git init
 git add -A
-git commit -m "deploy, $commit_info"
+read -p "enter the commit for this push: " commit
+git commit -m "deploy, $commit"
 git push -f $push_addr HEAD:$push_branch
 
 cd -
---
+++ ops.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+function push_function() {
+    read -p "enter the commit for this push: " commit
+    git add .
+    git commit -m "$commit"
+    git push
+}
+
+
+
+function print_menu() {
+	# clear  # 清屏使界面更清爽（可选）
+    echo -e "\033[34m===================================\033[0m"
+	echo -e "\033[32m[The Operations of This Repository]:\033[0m"
+    echo "1.dev"
+    echo "2.build"
+    echo "3.push"
+	echo "4.push again"
+    echo "5.pull"
+    echo "6.push to io"
+    echo -e "\033[34m===================================\033[0m"
+}
+
+
+while true; do
+    
+	print_menu
+    
+    read -p "enter the operation (1-6, or c to exit): " n
+
+    case $n in
+        1) 
+			echo "Running the Project..."
+			npm run dev:win
+			;;
+        2)
+			echo "Building the Project..."
+			npm run build:win
+			;;
+        3)
+			push_function ;;
+		4)
+			git push ;;
+        5) 
+			read -p "确认要从远程拉取更新吗？(y/n) " confirm
+			[[ $confirm == [yY] ]] && git pull || echo "已取消"
+			;;
+        6) 
+			sh deploy.sh ;;
+		c)
+			echo "Exiting..."; exit 0 ;;
+        *)
+			echo "无效输入，请重试"; sleep 1; continue ;;
+    esac
+
+    # 操作完成后暂停1秒（可选）
+    # sleep 1
+done
